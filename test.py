1bf81acc96d69cab01f757cb1f412989b18383ce
nick <trojanhorse0077@gmail.com>
2020-11-08 10:40:33 +0800

    優化查詢準確率到90%

--- test.py
+++ test.py
@@ -3,8 +3,6 @@ import json
 import operator
 from collections import defaultdict, Counter
 import time
-from itertools import groupby
-from operator import itemgetter
 
 from tokenize_service import tokenize
 
@@ -31,7 +29,7 @@ def case_1():
     print(i.search('july'))
 
 
-def saveJson_writeline(data, file_path):
+def saveJson(data, file_path):
     with codecs.open(file_path, mode="w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, default=lambda x: x.__dict__)
 
@@ -49,11 +47,11 @@ def case_2():
         for line in file:
             r1 = line.replace("\n", "").split('|')
             dic[r1[0]] = list(map(lambda x: int(x), r1[1].split(',')))
-    saveJson_writeline(dic, "./dataset/wiki_db_art_final_v2_j.json")
+    saveJson(dic, "./dataset/wiki_db_art_final_v2_j.json")
     # saveJson_writeline(dic,"./dataset/test_j.json")
 
 
-def case_3():
+def test_calc_score():
     start = time.time()
     j = load_json("./dataset/wiki_db_art_final_v2_j.json")
     # j = load_json("./dataset/test_j.json")
@@ -90,45 +88,74 @@ def case_3():
         "是中華人民共和國西南地區的一個省份，省會成都市。四川簡稱川或蜀，又因先秦時四川曾分屬巴國、蜀國兩諸侯國，故別稱「巴蜀」。四川歷史悠久、風光秀麗、物產豐富，自古被譽為「天府之國」。")
     ans_tokenized_6 = ["四川省", "遼寧省", "河北省"]
 
-    # ques_tokenized_7 = tokenize(
-    #     "明末清初著名軍事將領，曾因「引清兵入關」而被世人斥責為漢奸，他的名字叫做:")
-    # ans_tokenized_7 = ["吳一桂", "吳二桂", "吳三桂"]
+    ques_tokenized_7 = tokenize(
+        "位於太平洋西南部的一個島嶼國家，首都為威靈頓，但最大的城市為奧克蘭都會區。紐西蘭主要由兩大島嶼組成，即北島和南島，兩島以庫克海峽分隔，首都威靈頓即位於北島末端處，除此之外還包含了一些其他小的島嶼。")
+    ans_tokenized_7 = ["紐西蘭", "加拿大", "印度"]
+
+    ques_tokenized_8 = tokenize(
+        "西非國家，位於非洲的幾內亞灣西岸頂點，鄰國包括西邊的貝南，北邊的尼日，東北方與查德接壤一小段國界，正東則是喀麥隆。是全非洲人口最多的國家，首都原本為西南沿海的海港城市拉哥斯，1991年12月遷都至地理位置位居全國國土正中央的阿布札。")
+    ans_tokenized_8 = ["瑞士", "奈及利亞", "奧地利"]
 
     start_total = time.time()
+    calc_score(j, ques_tokenized_8, ans_tokenized_8)
+    end_total = time.time()
+    print('run_time = ', end_total - start_total)
+
+
+def case_7():
+    start = time.time()
+    j = load_json("./dataset/wiki_db_art_final_v2_j.json")
+    # j = load_json("./dataset/test_j.json")
+    end = time.time()
+    print('load = ', end - start)
+
+    question = load_json("./dataset/question.json")
+
+    start_total = time.time()
+    answers = [calc_score(j, tokenize(q["Question"]), [q["A"], q["B"], q["C"]]) for q in question]
+    end_total = time.time()
+    print('run_time = ', end_total - start_total)
+    print(json.dumps(answers))
+
+
+def calc_score(j, ques_tokenized, ans_tokenized):
+    start = time.time()
+    ans_index = get_model_indexted_res(j, ans_tokenized)
+    end = time.time()
+    # print('get ans_dic = ', end - start)
+    if len(ans_index.keys()) == 1:
+        print(ans_index.keys())
+        return get_ABC(list(ans_index.keys())[0], ans_tokenized)
 
     start = time.time()
-    ans_dic = get_model_indexted_res(j, ans_tokenized_6)
+    ques_index = get_model_indexted_res(j, ques_tokenized)
     end = time.time()
-    print('get ans_dic = ', end - start)
-    if len(ans_dic.items()) == 1:
-        print(ans_dic.keys())
-        return
-
-    start = time.time()
-    ques_dic = get_model_indexted_res(j, ques_tokenized_6)
-    end = time.time()
-    print('get ques_dic = ', end - start)
+    # print('get ques_dic = ', end - start)
 
     start = time.time()
     l_2 = []
-    # ques_dic_values = list(ques_dic.values())
-    # list(set(list1).intersection(set(list2),set(list3),....))
-    # ll = set(ques_dic_values[0]).intersection(*ques_dic_values)
-    #
-    # print("ll=", ll)
-    # result = set(lists[0]).intersection(*lists)
-    for i in ques_dic.values():
+    for i in ques_index.values():
         l_2 += i
 
     quest_counter = {x: count for x, count in Counter(l_2).items()}
 
-    answer = calc_ans(quest_counter, ans_dic)
+    answer = calc_ans(quest_counter, ans_index)
     end = time.time()
-    print('get answer = ', end - start)
+    # print('get answer = ', end - start)
 
-    end_total = time.time()
-    print('run_time = ', end_total - start_total)
-    print(answer)
+    print("===", answer, "===", end="\n\n")
+    return get_ABC(answer, ans_tokenized)
+
+
+def get_ABC(answer, ans_tokenized):
+    if ans_tokenized.__contains__(answer):
+        ans_tokenized_idx = ans_tokenized.index(answer)
+        if ans_tokenized_idx == 0:
+            return "A"
+        elif ans_tokenized_idx == 1:
+            return "B"
+
+    return "C"
 
 
 def process_tokenized(tokenized):
@@ -146,7 +173,7 @@ def process_wiki_db_wordcount():
         for line in file:
             r1 = line.replace("\n", "").split(' ')
             dic[r1[0]] = int(r1[1])
-        saveJson_writeline(dic, "./dataset/wiki_db_wordcount_j.json")
+        saveJson(dic, "./dataset/wiki_db_wordcount_j.json")
 
 
 def get_model_indexted_res(j, tokenized):
@@ -154,11 +181,10 @@ def get_model_indexted_res(j, tokenized):
     for key in tokenized:
         if key in j:
             res_dic[key] = j[key]
-
     return res_dic
 
 
-def case_4():
+def test_tokenize():
     tokenized = tokenize(
         "簡稱雲或滇，是中華人民共和國西南部邊疆地區的一個省份，省會昆明。雲南是人類重要的發祥地之一，生活在距今170萬年前的雲南元謀猿人，是迄今為止發現的中國乃至亞洲最早的人類。戰國時期，這裡是滇族部落的生息之地。")
     print(tokenized)
@@ -171,60 +197,26 @@ def tokenized(sentence):
 def calc_ans(ll, ans_dic):
     score_dic = {}
     score = 0
-    # ll = []
-    #
-    # for l in ques_dic.values():
-    #     ll+=l
 
     for a in ans_dic.items():
         start = time.time()
         for input_v in a[1]:
             score += get_score_of_inputV(input_v, ll)
         end = time.time()
-        print("get_score_of_inputV time = ", end - start)
-        print(a[0], 'score_tal=', str(score))
-        score_dic[a[0]] = score
+        # print("get_score_of_inputV time = ", end - start)
+        print(a[0], 'score_tal=', str(score / len(a[1])))
+        score_dic[a[0]] = score / len(a[1])
         score = 0
     return max(score_dic.items(), key=operator.itemgetter(1))[0]
 
 
 def get_score_of_inputV(input_v, ll):
-    score = 0
-    # ll=[]
-    # for ques_tuple in ques_dic.values():
-    # ques_vals = ques_tuple[1]
-    # ques_vals = ques_tuple
-    # ll+=ques_tuple
-    # r = list(ques_vals, filter(lambda x: x == v, ques_vals))
-    # r = list(filter(lambda x: x == input_v, ques_vals))
-    # score +=len(r)
-    # score += len(list(filter(lambda x: x == input_v, ques_vals)))
-    # print(score)
-    # print(sum(r))
-    # return len(set(filter(lambda x: x == input_v, ll)))
-    # print(len(ll))
-
-    # for v in ll:
-    #     if input_v == v:
-    #         score += 1
     if input_v in ll:
-        # return 1
         return ll[input_v]
-
     return 0
-    # print('input_v=', input_v, 'score=', score)
 
 
 def case_5():
-    # d_1 = {"吳一桂": [1, 4, 6, 7, 88]}
-    # d_2 = {"吳二桂": [1, 88, 3, 7, 77]}
-    # d_3 = {"吳三桂": [1, 99]}
-    # d_list = [d_1, d_2, d_3]
-    #
-    # ans_a = {"A": [99, 44]}
-    # ans_b = {"B": [14, 33, 88, 77]}
-    # ans_list = [ans_a, ans_b]
-
     ques_dic = {"吳一桂": [1, 4, 6, 7, 88],
                 "吳二桂": [1, 88, 3, 7, 77],
                 "吳三桂": [1, 99]}
@@ -241,32 +233,20 @@ def case_5():
         score_dic[a[0]] = score
         score = 0
 
-    # score_dic = {}
-    # for a in ans_list:
-    #     for v_list in a.values():
-    #         for v in v_list:
-    #             score += get_score_of_inputV(v, d_list)
-    #         print(a, 'score_tal=', str(score))
-    #         score_dic[v_list] = score
-    #         score = 0
-
 
 def case_6():
     lists = [[1, 4, 7], [4, 5, 8], [4, 8, 0]]
-    retB = []
     result = set(lists[0]).intersection(*lists)
     print(result)
-    # for i in range(len(lists)-3):
-    #     retB += list(set(first).intersection(set(lists[i+1])))
-    # print(retB)
 
 
 def main():
     # case_2()
-    case_3()
-    # case_4()
+    # test_calc_score()
+    # test_tokenize()
     # case_5()
     # case_6()
+    case_7()
     # process_wiki_db_wordcount()
     # process_tokenized(['簡稱', '雲', '滇', '中華人民共和國', '西南部', '邊疆地區', '一個', '省份', '省會', '昆明', '雲南', '人類', '重要', '發祥地', '生活', '距今', '170', '萬年前', '雲南', '元謀猿人', '迄今', '為止', '發現', '中國', '亞洲', '最早', '人類', '戰國時期', '滇', '族', '部落', '生息'])
 
